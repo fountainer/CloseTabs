a040a83d3556ee98bf5313632320abb9dea11752
fountainer <fountainer@163.com>
2017-09-23 01:31:00 +0800

    more groups of tabs

--- CloseTabs.py
+++ CloseTabs.py
@@ -2,33 +2,91 @@ import sublime, sublime_plugin
 import os
 
 class CloseTabsCommand(sublime_plugin.WindowCommand):
-    def run(self, type = None):
-        self.views = self.window.views()
-        if type == "left":
-            self.close_tabs_left()
-        elif type == "group":
-            self.close_tabs_group()
+    def close_tabs_left(self, arg):
+        if arg == "this":
+            view_active = self.window.active_view()
+            group_active, index_acitve = self.window.get_view_index(view_active)
+            views_left = []
+            for view in self.window.views():
+                group, index = self.window.get_view_index(view)
+                if group == group_active and index < index_acitve:
+                    views_left.append(view)
+            for view in views_left:
+                view.close()
+        elif arg == "all":
+            # cross groups
+            pass
+        else:
+            sublime.error_message(
+                'Undefined argument {} in "{}"'.format(arg, "close_tabs_left"))
 
-    def close_tabs_left(self):
-        view_active = self.window.active_view()
-        group_active, index_acitve = self.window.get_view_index(view_active)
-        views_left = []
-        for view in self.views:
-            group, index = self.window.get_view_index(view)
-            if group == group_active and index < index_acitve:
-                views_left.append(view)
-        for view in views_left:
-            view.close()
-
-    def close_tabs_group(self):
+    def close_tabs_group(self, arg):
         group_active = self.window.active_group()
-        views_group = [view for view in self.views 
-            if self.window.get_view_index(view)[0] == group_active]
+        views = self.window.views()
+        if arg == "this":
+            views_group = [view for view in views 
+                if self.window.get_view_index(view)[0] == group_active]
+        elif arg == "others":
+            views_group = [view for view in views 
+                if self.window.get_view_index(view)[0] != group_active]
+        else:
+            sublime.error_message(
+                'Undefined argument {} in "{}"'.format(arg, "close_tabs_group"))
         for view in views_group:
             view.close()
-        self.destroy_group(group_active)
+        # require Origami package
+        self.window.run_command("destroy_pane", {"direction": "self"})
 
-    def destroy_group(self, group):
-        pass
-        # use set_layout()
+    def close_tabs_folder(self, arg):
+        view_active = self.window.active_view()
+        file_name_active = view_active.file_name()
+        if file_name_active:
+            current_folder = os.path.dirname(file_name_active)
+        else:
+            sublime.error_message("This file does not exist on disk.")
+            return
+        num = self.window.num_groups()
+        num_views_in_groups = [0] * num
+        views = self.window.views()
+        for view in views:
+            group, _ = self.window.get_view_index(view)
+            num_views_in_groups[group] += 1
+        views_to_close = []
+        for view in views:
+            file_name = view.file_name()
+            if file_name:
+                if arg == "this" and os.path.dirname(file_name) == current_folder:
+                    pass
+                elif arg == "others" and os.path.dirname(file_name) != current_folder:
+                    pass
+                else:
+                    continue
+                views_to_close.append(view)
+                group, _ = self.window.get_view_index(view)
+                num_views_in_groups[group] -= 1
+            else:
+                pass
+        if not views_to_close:
+            return
+        for view in views_to_close:
+            view.close()
+        i = 0
+        while i < len(num_views_in_groups):
+            if num_views_in_groups[i] == 0:
+                del num_views_in_groups[i]
+                self.window.focus_group(i)
+                self.window.run_command("destroy_pane", {"direction": "self"})
+            else:
+                i = i + 1
 
+class CloseTabsLeftCommand(CloseTabsCommand):
+    def run(self, arg):
+        self.close_tabs_left(arg)
+
+class CloseTabsGroupCommand(CloseTabsCommand):
+    def run(self, arg):
+        self.close_tabs_group(arg)
+
+class CloseTabsFolderCommand(CloseTabsCommand):
+    def run(self, arg):
+        self.close_tabs_folder(arg)
--- README.md
+++ README.md
@@ -1,10 +1,15 @@
+## A sublime plugin to close tabs.
+
 ### Usage
 
 - Right click on the tab to close the tabs.
 - Or press `ctrl+shift+p` and type `close tabs` to select the actions that close tabs.
-- Current actions **Close Tabs to the Left**, **Close Tabs in this Group**.
+- Current actions **Tabs to the Left**, **Tabs in this Group**,
+**Tabs in other Groups**, **Tabs in Same Folder**, **Tabs in other Folders**.
 
 ### To Do
 
 - More actions.
-- Destroy groups.
+- Close tabs without files. (SublimeREPL)
+- Close other tabs cross groups.
+- Close scratch.
