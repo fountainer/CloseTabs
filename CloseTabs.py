849d3a3c70297f876ab39d7b8232d2b8164d2971
fountainer <fountainer@163.com>
2017-09-24 01:58:24 +0800

    handle with unsaved files

--- CloseTabs.py
+++ CloseTabs.py
@@ -2,17 +2,18 @@ import sublime, sublime_plugin
 import os
 
 class CloseTabsCommand(sublime_plugin.WindowCommand):
-    def close_tabs_left(self, arg):
+    def close_tabs_left(self, arg, save = None, skip = False):
         if arg == "this":
             view_active = self.window.active_view()
             group_active, index_acitve = self.window.get_view_index(view_active)
             views_left = []
             for view in self.window.views():
                 group, index = self.window.get_view_index(view)
-                if group == group_active and index < index_acitve:
+                if (group == group_active and index < index_acitve 
+                        and not self.is_skip_view(view, skip)):
                     views_left.append(view)
             for view in views_left:
-                view.close()
+                self.close_view(view, save)
         elif arg == "all":
             # cross groups
             pass
@@ -20,24 +21,36 @@ class CloseTabsCommand(sublime_plugin.WindowCommand):
             sublime.error_message(
                 'Undefined argument {} in "{}"'.format(arg, "close_tabs_left"))
 
-    def close_tabs_group(self, arg):
+    def close_tabs_group(self, arg, save = None, skip = False):
         group_active = self.window.active_group()
         views = self.window.views()
+        num_skip = 0
         if arg == "this":
-            views_group = [view for view in views 
-                if self.window.get_view_index(view)[0] == group_active]
+            views_group = []
+            for view in views:
+                if self.window.get_view_index(view)[0] == group_active:
+                    if self.is_skip_view(view, skip):
+                        num_skip += 1
+                    else:
+                        views_group.append(view)
         elif arg == "others":
-            views_group = [view for view in views 
-                if self.window.get_view_index(view)[0] != group_active]
+            views_group = []
+            for view in views:
+                if self.window.get_view_index(view)[0] != group_active:
+                    if self.is_skip_view(view, skip):
+                        num_skip += 1
+                    else:
+                        views_group.append(view)
         else:
             sublime.error_message(
                 'Undefined argument {} in "{}"'.format(arg, "close_tabs_group"))
         for view in views_group:
-            view.close()
+            self.close_view(view, save)
         # require Origami package
-        self.window.run_command("destroy_pane", {"direction": "self"})
+        if not num_skip:
+            self.window.run_command("destroy_pane", {"direction": "self"})
 
-    def close_tabs_folder(self, arg):
+    def close_tabs_folder(self, arg, save = None, skip = False):
         view_active = self.window.active_view()
         file_name_active = view_active.file_name()
         if file_name_active:
@@ -55,9 +68,13 @@ class CloseTabsCommand(sublime_plugin.WindowCommand):
         for view in views:
             file_name = view.file_name()
             if file_name:
-                if arg == "this" and os.path.dirname(file_name) == current_folder:
+                if (arg == "this" 
+                        and os.path.dirname(file_name) == current_folder 
+                        and not self.is_skip_view(view, skip)):
                     pass
-                elif arg == "others" and os.path.dirname(file_name) != current_folder:
+                elif (arg == "others" 
+                        and os.path.dirname(file_name) != current_folder
+                        and not self.is_skip_view(view, skip)):
                     pass
                 else:
                     continue
@@ -69,7 +86,7 @@ class CloseTabsCommand(sublime_plugin.WindowCommand):
         if not views_to_close:
             return
         for view in views_to_close:
-            view.close()
+            self.close_view(view, save)
         i = 0
         while i < len(num_views_in_groups):
             if num_views_in_groups[i] == 0:
@@ -79,14 +96,32 @@ class CloseTabsCommand(sublime_plugin.WindowCommand):
             else:
                 i = i + 1
 
+    def close_view(self, view, save = None):
+        if view.is_dirty():
+            if save is None:
+                pass
+            elif save:
+                view.run_command("save")
+            else:
+                view.set_scratch(True)
+        view.close()
+
+    def is_skip_view(self, view, skip = False):
+        if skip and view.is_dirty():
+            return True
+        else:
+            return False
+
+
+
 class CloseTabsLeftCommand(CloseTabsCommand):
-    def run(self, arg):
-        self.close_tabs_left(arg)
+    def run(self, arg, save = None, skip = False):
+        self.close_tabs_left(arg, save, skip)
 
 class CloseTabsGroupCommand(CloseTabsCommand):
-    def run(self, arg):
-        self.close_tabs_group(arg)
+    def run(self, arg, save = None, skip = False):
+        self.close_tabs_group(arg, save, skip)
 
 class CloseTabsFolderCommand(CloseTabsCommand):
-    def run(self, arg):
-        self.close_tabs_folder(arg)
+    def run(self, arg, save = None, skip = False):
+        self.close_tabs_folder(arg, save, skip)
--- README.md
+++ README.md
@@ -2,13 +2,16 @@
 
 ### Usage
 
-- Right click on the tab to close the tabs.
-- Or press `ctrl+shift+p` and type `close tabs` to select the actions that close tabs.
+- Select close tabs commands from tab menu or context menu.
+- Or press `ctrl+shift+p` and type `close tabs` to select close tabs commands.
 - Current actions **Tabs to the Left**, **Tabs in this Group**,
 **Tabs in other Groups**, **Tabs in Same Folder**, **Tabs in other Folders**.
+- For unsaved files, provide four ways to handle them: **Prompt for Unsaved**,
+**Skip Unsaved**, **Save Unsaved** and **Discard Unsaved**.
 
 ### To Do
 
+- The order of commands in menu.
 - More actions.
 - Close tabs without files. (SublimeREPL)
 - Close other tabs cross groups.
